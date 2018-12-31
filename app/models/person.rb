50f763d676435e4d587937bf4ac3f8b77f6f037f
malvov <manuel.olivaresbonilla@gmail.com>
2018-12-31 13:42:33 -0600

    person tests

--- app/models/person.rb
+++ app/models/person.rb
@@ -5,6 +5,9 @@ class Person < ApplicationRecord
     has_many :movies_as_producer, -> { PersonMovie.producer } , through: :person_movies, source: :movie
     accepts_nested_attributes_for :person_movies, reject_if: :all_blank
 
+    validates_presence_of :first_name, :last_name
+    validate :unique_person_full_name
+
     include PgSearch
     pg_search_scope :search, against: [:first_name, :last_name, :aliases], 
     using: {
@@ -20,5 +23,14 @@ class Person < ApplicationRecord
             'movies_as_director': movies_as_director.map { |movie| movie.info }, 
             'movies_as_producer': movies_as_producer.map { |movie| movie.info }
         }
+    end
+
+    private
+
+    def unique_person_full_name
+        if Person.where("LOWER(first_name) = ? AND LOWER(last_name) = ?",
+            first_name.downcase, last_name.downcase).exists?
+            errors.add(:base, 'Person with such info already exists')
+        end
     end
 end
---
+++ test/models/person_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class PersonTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+  def setup
+    @person = Person.new(first_name: 'Manuel', last_name: 'Olivares')
+  end
+
+  test 'should be valid' do
+    assert @person.valid?
+  end
+
+  test 'person info should be present' do
+    @person.first_name = ''
+    @person.last_name = ''
+    assert_not @person.valid?
+  end
+
+  test 'should reject people with same first name and last name' do
+    @person.save
+    @second_person = Person.new(first_name: 'Manuel', last_name: 'Olivares')
+    @third_person = Person.new(first_name: 'mAnuEl', last_name: 'oLivares')
+    assert_not @second_person.valid? && @third_person.valid?
+  end
+end

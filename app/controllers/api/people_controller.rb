dfd50c634e0565cd5c8078082810c83c34516c0e
malvov <manuel.olivaresbonilla@gmail.com>
2019-01-05 00:05:12 -0600

    handling record not found error

--- app/controllers/api/people_controller.rb
+++ app/controllers/api/people_controller.rb
@@ -48,8 +48,8 @@ module API
     private
       # Use callbacks to share common setup or constraints between actions.
       def set_person
-        @person = Person.find(params[:id])
-      end
+          @person = Person.find(params[:id])
+      end 
 
       # Only allow a trusted parameter "white list" through.
       def person_params
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::API
+    rescue_from ActiveRecord::RecordNotFound, with: :handle_error
+
     include Knock::Authenticable
+
+    private
+
+    def handle_error(e)
+      render json: { error: e.to_s }, status: :bad_request
+    end
 end
